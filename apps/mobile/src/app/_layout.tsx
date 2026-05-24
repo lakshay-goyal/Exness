00b77cfece76dc947f17ad848e499f6000a67cb3
Lakshay Goyal <112741354+lakshay-goyal@users.noreply.github.com>
2026-05-24 18:14:28 +0530

    setup the splash screen logic.

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,6 +1,18 @@
 import "../global.css"
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+SplashScreen.setOptions({
+  duration: 500,
+  fade: true,
+});
 
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <Stack />;
 }
